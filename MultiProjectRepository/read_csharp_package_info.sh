dbf324fcb05b8cdf17a69e27cbb9f29bc65bef1a
Konstantin Diachenko <drakonard@gmail.com>
2020-02-23 17:40:45 +0300

    Script renamed, added export for variables.

---
+++ MultiProjectRepository/read_csharp_package_info.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+sudo apt-get install xmlstarlet
+
+export CSHARP_PACKAGE_PROJECT_PATH="csharp/Platform.$REPOSITORY_NAME/Platform.$REPOSITORY_NAME.csproj"
+export CSHARP_PACKAGE_VERSION=$(xmlstarlet sel -t -m '//VersionPrefix[1]' -v . -n <"$PROJECT_PATH")
+export CSHARP_PACKAGE_RELEASE_NOTES=$(xmlstarlet sel -t -m '//PackageReleaseNotes[1]' -v . -n <"$PROJECT_PATH")
