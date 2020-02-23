41c4fd82e0a3ae7ac43dac609de3bce47218c304
Konstantin Diachenko <drakonard@gmail.com>
2020-02-23 17:47:08 +0300

    Script fix.

--- MultiProjectRepository/read_csharp_package_info.sh
+++ MultiProjectRepository/read_csharp_package_info.sh
@@ -4,5 +4,5 @@ set -e # Exit with nonzero exit code if anything fails
 sudo apt-get install xmlstarlet
 
 export CSHARP_PACKAGE_PROJECT_PATH="csharp/Platform.$REPOSITORY_NAME/Platform.$REPOSITORY_NAME.csproj"
-export CSHARP_PACKAGE_VERSION=$(xmlstarlet sel -t -m '//VersionPrefix[1]' -v . -n <"$PROJECT_PATH")
-export CSHARP_PACKAGE_RELEASE_NOTES=$(xmlstarlet sel -t -m '//PackageReleaseNotes[1]' -v . -n <"$PROJECT_PATH")
+export CSHARP_PACKAGE_VERSION=$(xmlstarlet sel -t -m '//VersionPrefix[1]' -v . -n <"$CSHARP_PACKAGE_PROJECT_PATH")
+export CSHARP_PACKAGE_RELEASE_NOTES=$(xmlstarlet sel -t -m '//PackageReleaseNotes[1]' -v . -n <"$CSHARP_PACKAGE_PROJECT_PATH")
