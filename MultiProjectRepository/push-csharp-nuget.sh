6ac60b4ab491f0a81756ea16c7c3840034a6db2c
Konstantin Dyachenko <konard@me.com>
2020-02-23 16:43:16 +0300

    Update push-csharp-nuget.sh

--- MultiProjectRepository/push-csharp-nuget.sh
+++ MultiProjectRepository/push-csharp-nuget.sh
@@ -4,28 +4,8 @@ set -e # Exit with nonzero exit code if anything fails
 # Pack NuGet package
 dotnet pack -c Release
 
-# Get version string
-PackageFileNamePrefix="csharp/Platform.$REPOSITORY_NAME/bin/Release/Platform.$REPOSITORY_NAME."
-PackageFileNameSuffix=".nupkg"
-PackageFileName=$(echo "$PackageFileNamePrefix"*"$PackageFileNameSuffix")
-Version="${PackageFileName#$PackageFileNamePrefix}"
-Version="${Version%$PackageFileNameSuffix}"
-
-# Ensure NuGet package does not exist
-NuGetPackageUrl="https://globalcdn.nuget.org/packages/Platform.$REPOSITORY_NAME.$Version$PackageFileNameSuffix"
-NuGetPackageUrl=$(echo "$NuGetPackageUrl" | tr '[:upper:]' '[:lower:]')
-
-# URL output for debug
-echo "$NuGetPackageUrl"
-
-NuGetPageStatus="$(curl -Is "$NuGetPackageUrl" | head -1)"
-StatusContents=( $NuGetPageStatus )
-if [ "${StatusContents[1]}" == "200" ]; then
-  echo "NuGet with current version is already pushed."
-else
-  # Push NuGet package
-  dotnet nuget push ./**/*.nupkg -s https://api.nuget.org/v3/index.json -k "${NUGETTOKEN}" 
-fi
+# Push NuGet package
+dotnet nuget push ./**/*.nupkg -s https://api.nuget.org/v3/index.json --skip-duplicate -k "${NUGETTOKEN}" 
 
 # Clean up
 find . -type f -name '*.nupkg' -delete
