1971a76ec015bf886cbf3f27c31e8890cf689b7f
Konstantin Diachenko <drakonard@gmail.com>
2020-03-03 21:46:44 +0300

    Better C++ NuGet package pushing.

--- MultiProjectRepository/push-cpp-nuget.sh
+++ MultiProjectRepository/push-cpp-nuget.sh
@@ -3,28 +3,21 @@ set -e # Exit with nonzero exit code if anything fails
 
 sudo apt-get install nuget
 
-##PathToPackageSource="cpp/Platform.$REPOSITORY_NAME"
-
-# Get version string
-#PackageSpecFileNamePrefix="$PathToPackageSource/Platform.$REPOSITORY_NAME.TemplateLibrary."
-#PackageSpecFileNameSuffix=".nuspec"
-#PackageSpecFileName=$(echo "$PackageSpecFileNamePrefix"*"$PackageSpecFileNameSuffix")
-##PackageSpecFileName="$PathToPackageSource/Platform.$REPOSITORY_NAME.TemplateLibrary.nuspec"
-#Version="${PackageSpecFileName#$PackageSpecFileNamePrefix}"
-#Version="${Version%$PackageSpecFileNameSuffix}"
+CPP_PACKAGE_NUSPEC_PATH=$(<CPP_PACKAGE_NUSPEC_PATH.txt)
+CPP_PACKAGE_VERSION=$(<CPP_PACKAGE_VERSION.txt)
 
 # Ensure NuGet package does not exist
-#NuGetPackageUrl="https://globalcdn.nuget.org/packages/Platform.$REPOSITORY_NAME.TemplateLibrary.$Version.nupkg"
-#NuGetPackageUrl=$(echo "$NuGetPackageUrl" | tr '[:upper:]' '[:lower:]')
-#NuGetPageStatus="$(curl -Is "$NuGetPackageUrl" | head -1)"
-#StatusContents=( $NuGetPageStatus )
-#if [ "${StatusContents[1]}" == "200" ]; then
-#  echo "NuGet with current version is already pushed."
-#  exit 0
-#fi
+NuGetPackageUrl="https://globalcdn.nuget.org/packages/Platform.$REPOSITORY_NAME.TemplateLibrary.$CPP_PACKAGE_VERSION.nupkg"
+NuGetPackageUrl=$(echo "$NuGetPackageUrl" | tr '[:upper:]' '[:lower:]')
+NuGetPageStatus="$(curl -Is "$NuGetPackageUrl" | head -1)"
+StatusContents=( $NuGetPageStatus )
+if [ "${StatusContents[1]}" == "200" ]; then
+  echo "NuGet with current version is already pushed."
+  exit 0
+fi
 
 # Pack NuGet package
-nuget pack "cpp/Platform.$REPOSITORY_NAME/Platform.$REPOSITORY_NAME.TemplateLibrary.nuspec"
+nuget pack "$CPP_PACKAGE_NUSPEC_PATH"
 
 # Push NuGet package
 nuget push ./**/*.nupkg -NoSymbols -Source https://api.nuget.org/v3/index.json -ApiKey "${NUGETTOKEN}" 
