ddb6bd2853c1099eefccde174d35057db8443e36
Konstantin Dyachenko <konard@me.com>
2020-03-03 21:19:54 +0300

    Attempt to simplify the script for C++ NuGet package publishing.

--- MultiProjectRepository/push-cpp-nuget.sh
+++ MultiProjectRepository/push-cpp-nuget.sh
@@ -3,32 +3,25 @@ set -e # Exit with nonzero exit code if anything fails
 
 sudo apt-get install nuget
 
-PathToPackageSource="cpp/Platform.$REPOSITORY_NAME/NuGetPackageSource"
+PathToPackageSource="cpp/Platform.$REPOSITORY_NAME"
 
 # Get version string
-PackageSpecFileNamePrefix="$PathToPackageSource/Platform.$REPOSITORY_NAME.TemplateLibrary."
-PackageSpecFileNameSuffix=".nuspec"
-PackageSpecFileName=$(echo "$PackageSpecFileNamePrefix"*"$PackageSpecFileNameSuffix")
-Version="${PackageSpecFileName#$PackageSpecFileNamePrefix}"
-Version="${Version%$PackageSpecFileNameSuffix}"
+#PackageSpecFileNamePrefix="$PathToPackageSource/Platform.$REPOSITORY_NAME.TemplateLibrary."
+#PackageSpecFileNameSuffix=".nuspec"
+#PackageSpecFileName=$(echo "$PackageSpecFileNamePrefix"*"$PackageSpecFileNameSuffix")
+PackageSpecFileName="$PathToPackageSource/$Platform.$REPOSITORY_NAME.TemplateLibrary.nuspec"
+#Version="${PackageSpecFileName#$PackageSpecFileNamePrefix}"
+#Version="${Version%$PackageSpecFileNameSuffix}"
 
 # Ensure NuGet package does not exist
-NuGetPackageUrl="https://globalcdn.nuget.org/packages/Platform.$REPOSITORY_NAME.TemplateLibrary.$Version.nupkg"
-NuGetPackageUrl=$(echo "$NuGetPackageUrl" | tr '[:upper:]' '[:lower:]')
-NuGetPageStatus="$(curl -Is "$NuGetPackageUrl" | head -1)"
-StatusContents=( $NuGetPageStatus )
-if [ "${StatusContents[1]}" == "200" ]; then
-  echo "NuGet with current version is already pushed."
-  exit 0
-fi
-
-# Ensure target directory exists
-LibNativeIncludeDirectory="$PathToPackageSource/lib/native/include"
-mkdir -p "$LibNativeIncludeDirectory"
-
-# Copy files
-find "cpp/Platform.$REPOSITORY_NAME" -maxdepth 1 -name \*.h -exec cp {} "$LibNativeIncludeDirectory/" \;
-find "cpp/Platform.$REPOSITORY_NAME" -maxdepth 1 -name \*.cpp -exec cp {} "$LibNativeIncludeDirectory/" \;
+#NuGetPackageUrl="https://globalcdn.nuget.org/packages/Platform.$REPOSITORY_NAME.TemplateLibrary.$Version.nupkg"
+#NuGetPackageUrl=$(echo "$NuGetPackageUrl" | tr '[:upper:]' '[:lower:]')
+#NuGetPageStatus="$(curl -Is "$NuGetPackageUrl" | head -1)"
+#StatusContents=( $NuGetPageStatus )
+#if [ "${StatusContents[1]}" == "200" ]; then
+#  echo "NuGet with current version is already pushed."
+#  exit 0
+#fi
 
 # Pack NuGet package
 nuget pack "$PackageSpecFileName"
