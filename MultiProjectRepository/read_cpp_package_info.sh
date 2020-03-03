2ac1c500d9107629c677e2b42cf8591651b28f63
Konstantin Dyachenko <konard@me.com>
2020-03-03 23:29:13 +0300

    Rule fix.

--- MultiProjectRepository/read_cpp_package_info.sh
+++ MultiProjectRepository/read_cpp_package_info.sh
@@ -4,9 +4,9 @@ set -e # Exit with nonzero exit code if anything fails
 CPP_PACKAGE_NUSPEC_PATH="cpp/Platform.$REPOSITORY_NAME/Platform.$REPOSITORY_NAME.TemplateLibrary.nuspec"
 echo "$CPP_PACKAGE_NUSPEC_PATH"
 echo "$CPP_PACKAGE_NUSPEC_PATH" > CPP_PACKAGE_NUSPEC_PATH.txt
-CPP_PACKAGE_VERSION=$(grep -Pzo "<version>[^<>]+</version>" "$CPP_PACKAGE_NUSPEC_PATH" | sed -e 's/<.\?[a-z]\+>//g')
+CPP_PACKAGE_VERSION=$(grep -Pzo "<version>[^<>]+</version>" "$CPP_PACKAGE_NUSPEC_PATH" | sed -e 's/<.\?[a-zA-Z]\+>//g')
 echo "$CPP_PACKAGE_VERSION"
 echo "$CPP_PACKAGE_VERSION" > CPP_PACKAGE_VERSION.txt
-CPP_PACKAGE_RELEASE_NOTES=$(grep -Pzo "<releaseNotes>[^<>]+</releaseNotes>" "$CPP_PACKAGE_NUSPEC_PATH" | sed -e 's/<.\?[a-z]\+>//g')
+CPP_PACKAGE_RELEASE_NOTES=$(grep -Pzo "<releaseNotes>[^<>]+</releaseNotes>" "$CPP_PACKAGE_NUSPEC_PATH" | sed -e 's/<.\?[a-zA-Z]\+>//g')
 echo "$CPP_PACKAGE_RELEASE_NOTES"
 echo "$CPP_PACKAGE_RELEASE_NOTES" > CPP_PACKAGE_RELEASE_NOTES.txt
