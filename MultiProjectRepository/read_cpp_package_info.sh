e9df56e6966acdc475a7565a55c21a4d130e3d9e
Konstantin Dyachenko <konard@me.com>
2020-03-03 22:10:45 +0300

    XML paths fix.

--- MultiProjectRepository/read_cpp_package_info.sh
+++ MultiProjectRepository/read_cpp_package_info.sh
@@ -6,9 +6,9 @@ sudo apt-get install xmlstarlet
 CPP_PACKAGE_NUSPEC_PATH="cpp/Platform.$REPOSITORY_NAME/Platform.$REPOSITORY_NAME.TemplateLibrary.nuspec"
 echo "$CPP_PACKAGE_NUSPEC_PATH"
 echo "$CPP_PACKAGE_NUSPEC_PATH" > CPP_PACKAGE_NUSPEC_PATH.txt
-CPP_PACKAGE_VERSION=$(xmlstarlet sel -t -m '//version[1]' -v . -n <"$CPP_PACKAGE_NUSPEC_PATH")
+CPP_PACKAGE_VERSION=$(xmlstarlet sel -t -m '///version[1]' -v . -n <"$CPP_PACKAGE_NUSPEC_PATH")
 echo "$CPP_PACKAGE_VERSION"
 echo "$CPP_PACKAGE_VERSION" > CPP_PACKAGE_VERSION.txt
-CPP_PACKAGE_RELEASE_NOTES=$(xmlstarlet sel -t -m '//releaseNotes[1]' -v . -n <"$CPP_PACKAGE_NUSPEC_PATH")
+CPP_PACKAGE_RELEASE_NOTES=$(xmlstarlet sel -t -m '///releaseNotes[1]' -v . -n <"$CPP_PACKAGE_NUSPEC_PATH")
 echo "$CPP_PACKAGE_RELEASE_NOTES"
 echo "$CPP_PACKAGE_RELEASE_NOTES" > CPP_PACKAGE_RELEASE_NOTES.txt
