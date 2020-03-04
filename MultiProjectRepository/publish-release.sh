c58e8ac1e2dc3fc323902d0460113c374ff215df
Konstantin Dyachenko <konard@me.com>
2020-03-04 09:37:35 +0300

    Combine C# and C++ version into single tag name.

--- MultiProjectRepository/publish-release.sh
+++ MultiProjectRepository/publish-release.sh
@@ -4,19 +4,13 @@ set -e # Exit with nonzero exit code if anything failss
 CSHARP_PACKAGE_VERSION=$(<CSHARP_PACKAGE_VERSION.txt)
 CSHARP_PACKAGE_RELEASE_NOTES=$(<CSHARP_PACKAGE_RELEASE_NOTES.txt)
 
-TAG_ID=$(curl --request GET --url "https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/${CSHARP_PACKAGE_VERSION}" --header "authorization: Bearer ${GITHUB_TOKEN}" | jq -r '.id')
-
-if [ "$TAG_ID" != "null" ]; then
-    echo "Release with the same tag already published."
-    exit 0
-fi
-
 SEPARATOR="
 
 "
 
 CSHARP_PACKAGE_URL="https://www.nuget.org/packages/Platform.$REPOSITORY_NAME/$CSHARP_PACKAGE_VERSION"
 
+TAG_BUFFER="$CSHARP_PACKAGE_VERSION"
 PACKAGE_RELEASE_NOTES_STRING_BUFFER="$CSHARP_PACKAGE_URL$SEPARATOR$CSHARP_PACKAGE_RELEASE_NOTES"
 
 if [ -f "CPP_PACKAGE_VERSION.txt" ]; then
@@ -26,8 +20,16 @@ if [ -f "CPP_PACKAGE_VERSION.txt" ]; then
 
     CPP_PACKAGE_URL="https://www.nuget.org/packages/Platform.$REPOSITORY_NAME.TemplateLibrary/$CPP_PACKAGE_VERSION"
 
+    TAG_BUFFER="$TAG_BUFFER_$CPP_PACKAGE_VERSION"
     PACKAGE_RELEASE_NOTES_STRING_BUFFER="$PACKAGE_RELEASE_NOTES_STRING_BUFFER$SEPARATOR$SEPARATOR$CPP_PACKAGE_URL$SEPARATOR$CPP_PACKAGE_RELEASE_NOTES"
 
+fi
+
+TAG_ID=$(curl --request GET --url "https://api.github.com/repos/${GITHUB_REPOSITORY}/releases/tags/${TAG_BUFFER}" --header "authorization: Bearer ${GITHUB_TOKEN}" | jq -r '.id')
+
+if [ "$TAG_ID" != "null" ]; then
+    echo "Release with the same tag already published."
+    exit 0
 fi
 
 PACKAGE_RELEASE_NOTES_STRING=$(jq -saR . <<< "$PACKAGE_RELEASE_NOTES_STRING_BUFFER")
