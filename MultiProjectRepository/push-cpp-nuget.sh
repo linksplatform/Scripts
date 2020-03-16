89f17d104b69f64510d02b6e51fc16ea2c2b8fdb
Konstantin Dyachenko <konard@me.com>
2020-03-16 17:49:23 +0300

    Auto-download of package icon enabled.

--- MultiProjectRepository/push-cpp-nuget.sh
+++ MultiProjectRepository/push-cpp-nuget.sh
@@ -4,6 +4,7 @@ set -e # Exit with nonzero exit code if anything fails
 sudo apt-get install nuget
 
 CPP_PACKAGE_NUSPEC_PATH=$(<CPP_PACKAGE_NUSPEC_PATH.txt)
+CPP_PACKAGE_NUSPEC_DIRECTORY=$(dirname "$CPP_PACKAGE_NUSPEC_PATH")
 CPP_PACKAGE_VERSION=$(<CPP_PACKAGE_VERSION.txt)
 
 # Ensure NuGet package does not exist
@@ -15,6 +16,9 @@ if [ "${StatusContents[1]}" == "200" ]; then
   echo "NuGet with current version is already pushed."
   exit 0
 fi
+
+# Download icon.png for the package
+wget -O "$CPP_PACKAGE_NUSPEC_DIRECTORY/icon.png" https://raw.githubusercontent.com/linksplatform/Documentation/18469f4d033ee9a5b7b84caab9c585acab2ac519/doc/Avatar-rainbow-icon-64x64.png
 
 # Pack NuGet package
 nuget pack "$CPP_PACKAGE_NUSPEC_PATH"
