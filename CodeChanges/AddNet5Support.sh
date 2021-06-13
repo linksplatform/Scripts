37e7e8381afb656859040dffc9895cccda088b06
konard <drakonard@gmail.com>
2021-06-13 20:11:37 +0300

    Better version of script.

--- CodeChanges/AddNet5Support.sh
+++ CodeChanges/AddNet5Support.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e # Exit with nonzero exit code if anything fails
 
 # Use netcoreapp3.1 instead of netcoreapp3.0 (can be used separately)
 find . -type f -name "*.csproj" -print0 | xargs -0 sed -i -e 's/netcoreapp3\.0/netcoreapp3.1/g'
@@ -8,4 +7,7 @@ find . -type f -name "*.csproj" -print0 | xargs -0 sed -i -e 's/netcoreapp3\.0/n
 find . -type f -name "*.yml" -print0 | xargs -0 sed -i -e 's/dotnet test -c Release -f netcoreapp3.1/dotnet test -c Release -f net5/g'
 
 # Add net5 target framework
-find . -type f -name "*.csproj" -print0 | xargs -0 sed -i -e 's/netstandard2\.1</netstandard2.1;net5</g' -e 's/netcoreapp3.1</netcoreapp3.1;net5</g'
+find . -type f -name "*.csproj" -print0 | xargs -0 sed -i -e 's/netstandard2\.1</netstandard2.1;net5</g' 
+
+# Replace application's target framework
+find . -type f -name "*.csproj" -print0 | xargs -0 sed -i -e 's|<TargetFramework>netcoreapp3\.1<\/TargetFramework>|<TargetFramework>net5<\/TargetFramework>|g'
