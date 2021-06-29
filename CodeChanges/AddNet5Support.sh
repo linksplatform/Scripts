9a4ae4166e26d062068829256247cee755503e25
konard <drakonard@gmail.com>
2021-06-29 17:11:00 +0300

    * net5 framework moniker addition is safer now * .github folder is automatically removed from .gitignore now if AddNet5Support.sh is executed

--- CodeChanges/AddNet5Support.sh
+++ CodeChanges/AddNet5Support.sh
@@ -7,7 +7,10 @@ find . -type f -name "*.csproj" -print0 | xargs -0 sed -i -e 's/netcoreapp3\.0/n
 find . -type f -name "*.yml" -print0 | xargs -0 sed -i -e 's/dotnet test -c Release -f netcoreapp3.1/dotnet test -c Release -f net5/g'
 
 # Add net5 target framework
-find . -type f -name "*.csproj" -print0 | xargs -0 sed -i -e 's/netstandard2\.1</netstandard2.1;net5</g' 
+find . -type f -name "*.csproj" -print0 | xargs -0 sed -i -e 's|netstandard2\.1<\/TargetFrameworks|netstandard2.1;net5<\/TargetFrameworks|g' -e 's|netcoreapp3\.1<\/TargetFrameworks|netcoreapp3.1;net5<\/TargetFrameworks|g' 
 
 # Replace application's target framework
 find . -type f -name "*.csproj" -print0 | xargs -0 sed -i -e 's|<TargetFramework>netcoreapp3\.1<\/TargetFramework>|<TargetFramework>net5<\/TargetFramework>|g'
+
+# Remove .github folder from .gitignore
+find . -type f -name ".gitignore" -print0 | xargs -0 sed -i -z -e 's|\n# GitHub refuses chages to .github folder at the moment\n.github\/\n||g'
