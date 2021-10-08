903c91c1df07a7a2573110add22ea37d8ff71c14
FreePhoenix888 <66206278+FreePhoenix888@users.noreply.github.com>
2021-10-08 19:29:22 +0600

    Update and rename CommitAndPushToAllRepos to CommitAndPushToAllRepos.sh

--- CodeChanges/CommitAndPushToAllRepos.sh
+++ CodeChanges/CommitAndPushToAllRepos.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 # Make sure you are in the default branch
 default_branch=$(git remote show origin | sed -n '/HEAD branch/s/.*: //p');for subdirectory in ./*/; do printf "Repository name: ${subdirectory}"; cd $subdirectory; git commit -m "Message"; git push origin $default_branch; cd ..; done;
