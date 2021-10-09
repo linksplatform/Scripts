72fe28e17015d8a2d4ce40609e46145fc8d0975c
konard <drakonard@gmail.com>
2021-10-09 20:36:09 +0300

    Better version of automatic cpp header guards removal is added.

--- CodeChanges/RemoveCppHeaderGuards.sh
+++ CodeChanges/RemoveCppHeaderGuards.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . -type f -name "*.h" -print0 | xargs -0 perl -0777pi -e 's~#pragma once(\r|\n|\s)+#ifndef [A-Za-z_]+(\r|\n|\s)+#define [A-Za-z_]+(?<BODY>(.|\r|\n|\s)+[^\r\n\s])+(\r|\n|\s)+#endif~#pragma once$+{BODY}~gm'
+find . -type f -name "*.h" -print0 | xargs -0 perl -0777pi -e 's~#pragma once(\r|\n|\s)+#ifndef [A-Za-z_]+(\r|\n|\s)+#define [A-Za-z_]+(?<BODY>(.|\r|\n|\s)+[^\r\n\s])+(\r|\n|\s)+#endif((\r|\n|\s)+\/\/(\r|\n|\s)+[A-Za-z_]+(\r|\n|\s)+)~#pragma once$+{BODY}\n~gm'
