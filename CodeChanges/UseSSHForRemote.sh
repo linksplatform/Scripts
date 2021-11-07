17223f768693048e676f497c86f22e4b718cbb36
FreePhoenix888 <66206278+FreePhoenix888@users.noreply.github.com>
2021-11-07 21:07:49 +0600

    Update UseSSHForRemote.sh

--- CodeChanges/UseSSHForRemote.sh
+++ CodeChanges/UseSSHForRemote.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-repository_name=$(git remote show origin | grep 'Fetch URL.*' | perl -pe 's~.*\/~$1~g'); git remote remove origin; git remote add origin git@github.com:linksplatform/${repository_name}.git
+repository_name=$(git remote show origin | grep 'Fetch URL.*' | perl -pe 's~.*\/(?<repository_name>\..*~$+{repository_name}~g'); git remote remove origin; git remote add origin git@github.com:linksplatform/${repository_name}.git
