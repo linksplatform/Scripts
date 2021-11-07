31954479ce03d40770ad9ce8ce9b511efe9bc2cd
FreePhoenix888 <66206278+FreePhoenix888@users.noreply.github.com>
2021-11-07 20:31:32 +0600

    Update RemoveDocumentationForPrivateClassesAndMethods.sh

--- CodeChanges/RemoveDocumentationForPrivateClassesAndMethods.sh
+++ CodeChanges/RemoveDocumentationForPrivateClassesAndMethods.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . -name *.cs -exec perl -0777pi -e 's~(^\s*/{3}.+(\s)+)+(?<save>(((\[MethodImpl\(MethodImplOptions\.AggressiveInlining\)\])|(\[((Fact)|(Theory))\]))\s+)*^\s+(private|class))~$+{save}~gm' {}  \
+find . -name *.cs -exec perl -0777pi -e 's~(^\s*/{3}.+(\s)+)+(?<save>(((\[MethodImpl\(MethodImplOptions\.AggressiveInlining\)\])|(\[((Fact)|(Theory))\]))\s+)*^\s+(private|class))~$+{save}~gm' {}  \;
