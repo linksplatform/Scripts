345ab461581482691ec10eb4dcd7cb1ff6d1dc0e
konard <drakonard@gmail.com>
2021-11-09 09:22:08 +0300

    .NET 6.0 support addition script draft.

---
+++ CodeChanges/AddNet6Support.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Use net6 instead of net5 in automated tests
+find . -type f -name "*.yml" -print0 | xargs -0 sed -i -e 's/dotnet test -c Release -f net5/dotnet test -c Release -f net6/g'
+
+# Add net6 target framework
+find . -type f -name "*.csproj" -print0 | xargs -0 sed -i -e 's|net5<\/TargetFrameworks|net5;net6<\/TargetFrameworks|g' -e 's|net5<\/TargetFrameworks|net5;net6<\/TargetFrameworks|g' 
+
+# Replace application's target framework
+find . -type f -name "*.csproj" -print0 | xargs -0 sed -i -e 's|<TargetFramework>net5<\/TargetFramework>|<TargetFramework>net6<\/TargetFramework>|g'
