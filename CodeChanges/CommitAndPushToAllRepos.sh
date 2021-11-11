ac75921c77d4623d3b6e707070eeb739016c4910
Artem <vanty@mail.ru>
2021-11-12 03:44:21 +0900

    Delete file CodeChanges/CommitAndPushToAllRepos.sh

--- CodeChanges/CommitAndPushToAllRepos.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Make sure you are in the default branch
-for subdirectory in ./*/; do cd $subdirectory; default_branch=$(git remote show origin | sed -n '/HEAD branch/s/.*: //p');printf "Repository name: ${subdirectory}"; git commit -m "Message"; git push origin $default_branch; cd ..; done;
