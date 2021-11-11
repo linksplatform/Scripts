79d82eb6ec2259d32f52cbe877ae53d79c8ac1de
FreePhoenix888 <66206278+FreePhoenix888@users.noreply.github.com>
2021-11-11 22:50:11 +0600

    Update RevertCommitWithSpecificMessage.sh

--- CodeChanges/RevertCommitWithSpecificMessage.sh
+++ CodeChanges/RevertCommitWithSpecificMessage.sh
@@ -4,10 +4,9 @@
 commit_message="Commit message";
 
 revert() {
-  printf "Repository name: ${subdirectory}\n";
   cd $subdirectory;
   commit_id=$(git log --oneline | grep -o -m 1 ".*${commit_message}.*" | perl -pe "s~(?<commit_id>\S+).+~$+{commit_id}~g");
-  git revert ${commit_id};
+  [[ ${commit_id} != "" ]] && git revert --no-edit "${commit_id}";
   cd ..;
 }
 
