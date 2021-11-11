b1cbedc0fc4a033d5b5e799a6aa0758bafde403a
FreePhoenix888 <66206278+FreePhoenix888@users.noreply.github.com>
2021-11-11 13:14:14 +0600

    Update CloneAllOrganizationRepositoriesByHTTPS.sh

--- Utils/CloneAllOrganizationRepositoriesByHTTPS.sh
+++ Utils/CloneAllOrganizationRepositoriesByHTTPS.sh
@@ -2,8 +2,14 @@
 # Make sure perl and jq are installed
 
 repositories_json=$(curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/orgs/linksplatform/repos?per_page=100");
-for clone_url_with_double_quotes in $( printf "${repositories_json}" | jq '.[] | .clone_url' )
-do
+
+clone() {
   clone_url=$(printf ${clone_url_with_double_quotes} | perl -pe 's~"(?<clone_url>.*)"~$+{clone_url}~g');
   git clone ${clone_url};
+}
+
+for clone_url_with_double_quotes in $( printf "${repositories_json}" | jq '.[] | .clone_url' )
+do
+  clone &
 done;
+wait;
