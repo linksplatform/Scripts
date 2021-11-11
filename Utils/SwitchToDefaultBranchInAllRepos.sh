125ee63659f0e743128708098b2c7316f32d0b16
FreePhoenix888 <66206278+FreePhoenix888@users.noreply.github.com>
2021-11-11 11:15:34 +0600

    Update SwitchToDefaultBranchInAllRepos.sh

--- Utils/SwitchToDefaultBranchInAllRepos.sh
+++ Utils/SwitchToDefaultBranchInAllRepos.sh
@@ -3,7 +3,7 @@
 for subdirectory in ./*/
 do
   cd $subdirectory;
-  printf "Repository name: ${subdirectory}";
+  printf "Repository name: ${subdirectory}\n";
   default_branch=$(git remote show origin | sed -n '/HEAD branch/s/.*: //p');  
   git switch $default_branch;
   cd ..;
