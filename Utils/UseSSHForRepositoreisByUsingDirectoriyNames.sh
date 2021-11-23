02aedc34f8b319645b37d383a468ca9260ea73ec
FreePhoenix888 <66206278+FreePhoenix888@users.noreply.github.com>
2021-11-23 14:27:23 +0600

    Update UseSSHForRepositoreisByUsingDirectoriyNames.sh

--- Utils/UseSSHForRepositoreisByUsingDirectoriyNames.sh
+++ Utils/UseSSHForRepositoreisByUsingDirectoriyNames.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-for subdirectory in ./*/
-do
+use_ssh() {
   cd ${subdirectory};
   repository_name=$(printf "$subdirectory" | perl -pe 's~\./(?<repository_name>.*)\/~$+{repository_name}~g');
   git remote set-url origin git@github.com:linksplatform/${repository_name}.git;
   printf "\n${repository_name} - Done\n";
   cd ..;
+}
+
+for subdirectory in ./*/
+do
+  use_ssh &
 done;
