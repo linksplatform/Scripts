91df977b662b5a6c5805835cbb82e9600b5ef717
Konstantin Dyachenko <konard@me.com>
2021-11-27 12:41:53 +0300

    Added support for replacement of `net5.0`

--- CodeChanges/AddNet6Support.sh
+++ CodeChanges/AddNet6Support.sh
@@ -4,7 +4,7 @@
 find . -type f -name "*.yml" -print0 | xargs -0 sed -i -e 's/dotnet test -c Release -f net5/dotnet test -c Release -f net6/g'
 
 # Add net6 target framework
-find . -type f -name "*.csproj" -print0 | xargs -0 sed -i -e 's|net5<\/TargetFrameworks|net5;net6<\/TargetFrameworks|g' -e 's|net5<\/TargetFrameworks|net5;net6<\/TargetFrameworks|g' 
+find . -type f -name "*.csproj" -print0 | xargs -0 sed -i -e 's|net5<\/TargetFrameworks|net5;net6<\/TargetFrameworks|g' -e 's|net5<\/TargetFrameworks|net5;net6<\/TargetFrameworks|g' -e 's|net5.0<\/TargetFrameworks|net5;net6<\/TargetFrameworks|g' -e 's|net5.0<\/TargetFrameworks|net5;net6<\/TargetFrameworks|g'
 
 # Replace application's target framework
-find . -type f -name "*.csproj" -print0 | xargs -0 sed -i -e 's|<TargetFramework>net5<\/TargetFramework>|<TargetFramework>net6<\/TargetFramework>|g'
+find . -type f -name "*.csproj" -print0 | xargs -0 sed -i -e 's|<TargetFramework>net5<\/TargetFramework>|<TargetFramework>net6<\/TargetFramework>|g' -e 's|<TargetFramework>net5.0<\/TargetFramework>|<TargetFramework>net6<\/TargetFramework>|g'
