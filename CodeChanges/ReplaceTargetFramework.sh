e2d48118cf87014e87df9ef6dca2dab1b4b1e563
FreePhoenix888 <66206278+FreePhoenix888@users.noreply.github.com>
2022-02-16 21:16:17 +0600

    Update ReplaceTargetFramework.sh

--- CodeChanges/ReplaceTargetFramework.sh
+++ CodeChanges/ReplaceTargetFramework.sh
@@ -5,3 +5,6 @@ framework_regular_expression="netstandard2\.1"
 new_framework="netstandard3.1"
 
 find . -type f -name "*.csproj" -exec perl -0777pi -e "s~(?<before><TargetFrameworks>.*?)${framework_regular_expression};(?<after>.*?<\/TargetFrameworks>)~$+{before}${new_framework};$+{after}~" {} \;
+
+# Remove duplicates
+find . -type f -name "*.csproj" -exec perl -0777pi -e "s~(?<before><TargetFrameworks>.*?)${new_framework};(?<between>)${new_framework};(?<after>.*?<\/TargetFrameworks>)~$+{before}$+{between}${new_framework};$+{after}~" {} \;
