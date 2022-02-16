af47d332b7d8eee4acedc5cb6a7b62aba20ba731
FreePhoenix888 <66206278+FreePhoenix888@users.noreply.github.com>
2022-02-16 11:37:03 +0600

    Create ReplaceTargetFramework.sh

---
+++ CodeChanges/ReplaceTargetFramework.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Assign your framefork to this variable
+framework_regular_expression="netstandard2\.1" 
+new_framework="netstandard3.1"
+
+find . -type f -name "*.csproj" -exec perl -0777pi -e "s~(?<before><TargetFrameworks>.*?)${framework_regular_expression};(?<after>.*?<\/TargetFrameworks>)~$+{before}${new_framework}$+{after}~" {} \;
