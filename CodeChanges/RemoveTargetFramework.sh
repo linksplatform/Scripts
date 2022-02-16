7a266bb05744e68f6b29d29beb0b7bfd60b6239f
FreePhoenix888 <66206278+FreePhoenix888@users.noreply.github.com>
2022-02-16 11:36:09 +0600

    Update RemoveTargetFramework.sh

--- CodeChanges/RemoveTargetFramework.sh
+++ CodeChanges/RemoveTargetFramework.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Assign your framefork to this variable
-framework_regular_expression="netstandard2\.0" 
+framework_regular_expression="netstandard2\.1" 
 
 find . -type f -name "*.csproj" -exec perl -0777pi -e "s~(?<before><TargetFrameworks>.*?)${framework_regular_expression};(?<after>.*?<\/TargetFrameworks>)~$+{before}$+{after}~" {} \;
