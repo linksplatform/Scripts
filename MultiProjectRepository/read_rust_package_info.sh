9f98d2b0d81c5b976a1bb51b0c63b47c8e3adffe
Konstantin Dyachenko <drakonard@gmail.com>
2022-04-02 11:40:56 +0000

    read_rust_package_info script is added.

---
+++ MultiProjectRepository/read_rust_package_info.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+RUST_PACKAGE_PROJECT_PATH="rust/Cargo.toml"
+echo "$RUST_PACKAGE_PROJECT_PATH"
+echo "$RUST_PACKAGE_PROJECT_PATH" > RUST_PACKAGE_PROJECT_PATH.txt
+RUST_PACKAGE_VERSION=$(cat "$RUST_PACKAGE_PROJECT_PATH" | grep "^version = " | grep -Eo "([0-9]|\.)+")
+echo "$RUST_PACKAGE_VERSION"
+echo "$RUST_PACKAGE_VERSION" > RUST_PACKAGE_VERSION.txt
