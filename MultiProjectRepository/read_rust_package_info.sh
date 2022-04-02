c9cd8261c2fbb1640849102c0b34187733ebec15
Konstantin Dyachenko <drakonard@gmail.com>
2022-04-02 11:50:12 +0000

    Name is now read too.

--- MultiProjectRepository/read_rust_package_info.sh
+++ MultiProjectRepository/read_rust_package_info.sh
@@ -4,6 +4,9 @@ set -e # Exit with nonzero exit code if anything fails
 RUST_PACKAGE_PROJECT_PATH="rust/Cargo.toml"
 echo "$RUST_PACKAGE_PROJECT_PATH"
 echo "$RUST_PACKAGE_PROJECT_PATH" > RUST_PACKAGE_PROJECT_PATH.txt
+RUST_PACKAGE_NAME=$(cat "$RUST_PACKAGE_PROJECT_PATH" | grep -Po "name = \"\K[^\"]+" | head -1)
+echo "$RUST_PACKAGE_NAME"
+echo "$RUST_PACKAGE_NAME" > RUST_PACKAGE_NAME.txt
 RUST_PACKAGE_VERSION=$(cat "$RUST_PACKAGE_PROJECT_PATH" | grep "^version = " | grep -Eo "([0-9]|\.)+")
 echo "$RUST_PACKAGE_VERSION"
 echo "$RUST_PACKAGE_VERSION" > RUST_PACKAGE_VERSION.txt
