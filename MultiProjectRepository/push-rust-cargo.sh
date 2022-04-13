d2f4475bbc0e26d2c658b9bb79c09a574c486830
uselessgoddess <dodickgod@gmail.com>
2022-04-13 21:21:49 +0300

    Add cleaning to script

--- MultiProjectRepository/push-rust-cargo.sh
+++ MultiProjectRepository/push-rust-cargo.sh
@@ -4,6 +4,9 @@ set -e # Exit with nonzero exit code if anything fails
 RUST_PACKAGE_NAME=$(<RUST_PACKAGE_NAME.txt)
 RUST_PACKAGE_VERSION=$(head -n 1 RUST_PACKAGE_VERSION.txt)
 
+> RUST_PACKAGE_NAME.txt
+> RUST_PACKAGE_VERSION.txt
+
 # Ensure Cargo package does not exist
 CargoPackageUrl="https://crates.io/api/v1/crates/$RUST_PACKAGE_NAME/$RUST_PACKAGE_VERSION/download"
 CargoPageStatus="$(curl -ILs "$CargoPackageUrl")"
