16f3e109267285976256cb2b3f5538cd30d00009
FreePhoenix888 <66206278+FreePhoenix888@users.noreply.github.com>
2022-04-15 13:03:13 +0600

    Update move_sln_file_to_csharp_directory.sh

--- CodeChanges/move_sln_file_to_csharp_directory.sh
+++ CodeChanges/move_sln_file_to_csharp_directory.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 moveSlnToCsharpDirectory() {
-	for subdirectory in ./*/;
-	do
-		cd $subdirectory;
-		git stash;
-		git checkout main;
-		git pull;
-		repositoryName=$(basename "$subdirectory")
-		mv "Platform.${repositoryName}.sln" "csharp/";
-		git add *.sln;
-		git commit -m "Move sln file to csharp"
-		git push;
-		cd ..;
-	done;
+	cd $subdirectory;
+	git stash;
+	git checkout main;
+	git pull;
+	repositoryName=$(basename "$subdirectory")
+	mv "Platform.${repositoryName}.sln" "csharp/";
+	git add *.sln;
+	git commit -m "Move sln file to csharp"
+	git push;
+	cd ..;
 }
 
 
