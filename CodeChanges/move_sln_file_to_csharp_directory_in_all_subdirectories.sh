fc490b8870fddb6e6776c07cc60a74ac36c8b967
FreePhoenix888 <66206278+FreePhoenix888@users.noreply.github.com>
2022-04-16 17:15:35 +0600

    Update and rename move_sln_file_to_csharp_directory.sh to move_sln_file_to_csharp_directory_in_all_subdirectories.sh

---
+++ CodeChanges/move_sln_file_to_csharp_directory_in_all_subdirectories.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+moveSlnToCsharpDirectoryInAllSubdirectories() {
+	cd $subdirectory;
+	mv "Platform.${repositoryName}.sln" "csharp/";
+	cd ..;
+}
+
+
+for subdirectory in ./*/;
+do
+  moveSlnToCsharpDirectoryInAllSubdirectories &
+done;
+wait;
