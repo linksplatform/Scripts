1dc153b18a8053f0d4b0c9d0ee5b25dd91c7b7d0
FreePhoenix888 <66206278+FreePhoenix888@users.noreply.github.com>
2022-04-16 17:45:40 +0600

    Remove csharp from CSHARP_PACKAGE_PROJECT_PATH

--- MultiProjectRepository/read_csharp_package_info.sh
+++ MultiProjectRepository/read_csharp_package_info.sh
@@ -3,7 +3,7 @@ set -e # Exit with nonzero exit code if anything fails
 
 sudo apt-get install xmlstarlet
 
-CSHARP_PACKAGE_PROJECT_PATH="csharp/Platform.$REPOSITORY_NAME/Platform.$REPOSITORY_NAME.csproj"
+CSHARP_PACKAGE_PROJECT_PATH="Platform.$REPOSITORY_NAME/Platform.$REPOSITORY_NAME.csproj"
 echo "$CSHARP_PACKAGE_PROJECT_PATH"
 echo "$CSHARP_PACKAGE_PROJECT_PATH" > CSHARP_PACKAGE_PROJECT_PATH.txt
 CSHARP_PACKAGE_VERSION=$(xmlstarlet sel -t -m '//VersionPrefix[1]' -v . -n <"$CSHARP_PACKAGE_PROJECT_PATH")
