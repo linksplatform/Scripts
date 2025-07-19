b1c394021a7d310a0d92b5ae2146e6b98d638286
Konard <konard@example.com>
2025-07-19 21:33:48 +0530

    Make UpdateNet7ToNet8.sh script better.

--- CodeChanges/UpdateDotNetVersion/UpdateNet7ToNet8.sh
+++ CodeChanges/UpdateDotNetVersion/UpdateNet7ToNet8.sh
@@ -1,6 +1,8 @@
-sed -i '' 's/net7/net8/g' .github/**/*.yml
+sed -i '' 's/net7/net8/g' ./**/*/.github/**/*.yml
 
 sed -i '' 's/net7/net8/g' ./**/*.csproj
 
 # Remove duplicate target frameworks (e.g., net8;net8 -> net8)
-sed -i '' 's/<TargetFrameworks>net8;net8<\/TargetFrameworks>/<TargetFrameworks>net8<\/TargetFrameworks>/g' ./**/*.csproj
+sed -i '' 's|<TargetFrameworks>net8;net8</TargetFrameworks>|<TargetFramework>net8</TargetFramework>|g' ./**/*.csproj
+
+perl -0777 -pi -e 's|<PackageReleaseNotes>.*?</PackageReleaseNotes>|<PackageReleaseNotes>Update target framework from net7 to net8.</PackageReleaseNotes>|gs' ./**/*.csproj
